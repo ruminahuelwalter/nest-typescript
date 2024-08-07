6a6a5174d3ce672bc5b3c3453d5baca310b82ff0
ruminahuelwalter <ruminahuelwalter@gmail.com>
2024-08-07 15:01:44 -0300

    decoradores

--- 01-typescript-intro/src/main.ts
+++ 01-typescript-intro/src/main.ts
@@ -2,8 +2,9 @@ import './style.css'
 import typescriptLogo from './typescript.svg'
 import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.ts'
-import { charmander } from './bases/04-injection.ts'
+//import { charmander } from './bases/04-injection.ts'
 //import { name, age } from './bases/01-types.ts' 
+import { charmander } from './bases/05-decorators.ts'
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
     <a href="https://vitejs.dev" target="_blank">
